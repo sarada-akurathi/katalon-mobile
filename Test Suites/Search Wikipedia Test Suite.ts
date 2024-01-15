4310ac05e1bf20fdc5a90231dcd8567c526757ef
Sarada Akurathi <saradaakurathi@gmail.com>
2024-01-15 16:51:39 +0530

    add search wikipedia test

---
+++ Test Suites/Search Wikipedia Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Search Wikipedia Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>25ce0ece-092c-4552-87a9-95b2a75ae61b</testSuiteGuid>
+   <testCaseLink>
+      <guid>1f048af3-dcce-4f15-8063-d0d705566a3e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Search Wikipedia Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
